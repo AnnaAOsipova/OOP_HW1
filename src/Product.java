50bf8e2f490b317daede3b6c44303d0c87c9f6db
Anna <anna.a.osipova@outlook.com>
2025-01-26 01:25:40 +0200

    Initial commit

---
+++ src/Product.java
@@ -0,0 +1,30 @@
+public abstract class Product{
+
+    public Product(String name, int Price){
+        this.name = name;
+        this.price = price;
+    }
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "name='" + name + "\'" + ", price=" + price + "}";
+    }
+}
+
